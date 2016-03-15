3e325f8aa246e64c19dddbe7a7e7b4d406c78137
Yash Tanna <yashtanna93@gmail.com>
2016-03-15 01:58:00 -0700

    bug fix - Stock Hawk does not crash when a user searches for a non-existent stock.

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -4,14 +4,21 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.TaskParams;
+
+import android.os.Handler;
+import android.widget.Toast;
+
+import java.util.logging.LogRecord;
 
 /**
  * Created by sam_chordas on 10/1/15.
  */
 public class StockIntentService extends IntentService {
 
-  public StockIntentService(){
+  public StockIntentService() {
     super(StockIntentService.class.getName());
   }
 
@@ -19,15 +26,31 @@ public class StockIntentService extends IntentService {
     super(name);
   }
 
-  @Override protected void onHandleIntent(Intent intent) {
+  @Override
+  protected void onHandleIntent(Intent intent) {
     Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
     StockTaskService stockTaskService = new StockTaskService(this);
     Bundle args = new Bundle();
-    if (intent.getStringExtra("tag").equals("add")){
+    if (intent.getStringExtra("tag").equals("add")) {
       args.putString("symbol", intent.getStringExtra("symbol"));
     }
     // We can call OnRunTask from the intent service to force it to run immediately instead of
     // scheduling a task.
-    stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+    Handler handler = new Handler(getMainLooper());
+    if(stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args)) == GcmNetworkManager.RESULT_FAILURE) {
+      handler.post(new Runnable() {
+        @Override
+        public void run() {
+          Toast.makeText(getApplicationContext(), "Symbol Not Found!!", Toast.LENGTH_SHORT).show();
+        }
+      });
+    } else {
+      handler.post(new Runnable() {
+        @Override
+        public void run() {
+          Toast.makeText(getApplicationContext(), "Stock symbol added!", Toast.LENGTH_SHORT).show();
+        }
+      });
+    }
   }
 }
